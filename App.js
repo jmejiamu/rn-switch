db0715348fbe66ecfe0f7c7bd651029410fc1e55
jmejiamu <jmejiamu@gmail.com>
2022-06-04 22:08:33 -0700

    set text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Phone Bil</Text>
+      <Text>Phone Bill</Text>
       <StatusBar style="auto" />
     </View>
   );

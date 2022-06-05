0c013fd8970e97f067ffc18baaa2a1e175de3b8c
jmejiamu <jmejiamu@gmail.com>
2022-06-04 22:06:35 -0700

    added text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working Here!</Text>
+      <Text>Phone Bil</Text>
       <StatusBar style="auto" />
     </View>
   );
